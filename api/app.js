9f32b18d501b7933852411b6c12b156824bcceca
Austin Bundy <aubundy@gmail.com>
2026-01-23 15:01:51 -0600

    Add ui folder template

--- api/app.js
+++ api/app.js
@@ -3,8 +3,16 @@ import express from "express";
 // import { crawlTiles } from "./openstreetmap/service.js";
 import restaurantsRouter from "./restaurants/restaurants-routes.js";
 
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, "../ui/dist")));
 
 // app.get("/", async (req, res) => {
 //   await crawlTiles();
@@ -12,6 +20,9 @@ const port = 3000;
 // });
 
 app.use("/api", restaurantsRouter);
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../ui/dist", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
